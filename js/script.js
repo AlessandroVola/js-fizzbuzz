773d96e135b66915cebb2453cdfa30ec2dbfab4d
AlessandroVola <volaalessandro@gmail.com>
2022-08-26 00:24:23 +0200

    ciclo for semplice con mutipli di 3, 5 e entrambi

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+// inizio ciclo
+console.log('inizio');
+let i;
+for (i = 1; i < 101; i++) {
+
+    console.log(i);
+    // multiplo di 3
+    if (i % 3 == 0)
+        console.log("Fizz");
+    // multiplo di 5
+    if (i % 5 == 0)
+        console.log("Buzz");
+    // multiplo di 15
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+
+}
+
+
+
+
