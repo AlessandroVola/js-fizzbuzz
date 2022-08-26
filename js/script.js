d952c87ce83d168c59a0611bd1cb94d4cbe6fa07
AlessandroVola <volaalessandro@gmail.com>
2022-08-26 09:57:32 +0200

    accorgimenti

--- js/script.js
+++ js/script.js
@@ -2,18 +2,30 @@
 // inizio ciclo
 console.log('inizio');
 let i;
-for (i = 1; i < 101; i++) {
+for (i = 1; i < 100; i++) {
 
-    console.log(i);
-    // multiplo di 3
-    if (i % 3 == 0)
-        console.log("Fizz");
-    // multiplo di 5
-    if (i % 5 == 0)
-        console.log("Buzz");
     // multiplo di 15
-    if (i % 15 == 0)
+    if (i % 15 == 0) {
         console.log("FizzBuzz");
+    }
+
+    // multiplo di 3
+    else if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+
+    // multiplo di 5
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+
+    // resto dei numeri
+    else {
+        console.log(i)
+    }
+
+
+
 
 }
 
